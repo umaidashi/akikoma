7f55a54cc5bbdfd995ec224869f191bcdc0a7125
umaidashi <umaidashi18@gmail.com>
2023-05-21 13:58:03 +0900

    Update BottomNav.tsx

--- src/app/components/layout/BottomNav.tsx
+++ src/app/components/layout/BottomNav.tsx
@@ -30,7 +30,7 @@ export default function BottomNav() {
             ></path>
           </svg>
           <span
-            className={`text-sm text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
+            className={`text-xs text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
               path === "home" && "text-pink-600"
             }`}
           >
@@ -58,7 +58,7 @@ export default function BottomNav() {
             ></path>
           </svg>
           <span
-            className={`text-sm text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
+            className={`text-xs text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
               path === "group" && "text-pink-600"
             }`}
           >
@@ -82,7 +82,7 @@ export default function BottomNav() {
             <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
           </svg>
           <span
-            className={`text-sm text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
+            className={`text-xs text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
               path === "following" && "text-pink-600"
             }`}
           >
@@ -110,7 +110,7 @@ export default function BottomNav() {
             ></path>
           </svg>
           <span
-            className={`text-sm text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
+            className={`text-xs text-gray-500 dark:text-gray-400 group-hover:text-pink-600 dark:group-hover:text-pink-500 ${
               path === "mypage" && "text-pink-600"
             }`}
           >
