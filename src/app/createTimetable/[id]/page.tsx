55689e1220620435f02d9f85f02363885cedad90
umaidashi <umaidashi18@gmail.com>
2023-05-21 13:27:09 +0900

    hei

--- src/app/createTimetable/[id]/page.tsx
+++ src/app/createTimetable/[id]/page.tsx
@@ -3,7 +3,8 @@ import getTemplateTimetableById from "../../actions/getTemplateTimetableById";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
+  // const { id } = params;
+  const id = "clhvhq0ow00077lupqaq6h5w4";
   const data = await getTemplateTimetableById({ id });
   const templateTimetable = data?.userTemplateTimetable;
   const currentUser = await getCurrentUser();
--- src/app/createTimetable/page.tsx
+++ src/app/createTimetable/page.tsx
@@ -1,18 +1,35 @@
 import Link from "next/link";
 import getTemplateTimetables from "../actions/getTemplateTimetables";
+import CreateTimetable from "./[id]/CreateTimetable";
+import getCurrentUser from "../actions/getCurrentUser";
+import getTemplateTimetableById from "../actions/getTemplateTimetableById";
 
 export default async function Page() {
   const templateTimetables = await getTemplateTimetables();
+  // <div>
+  //   <div>createTimetable</div>
+  //   {templateTimetables.templateTimetables &&
+  //     templateTimetables.templateTimetables.map((t) => (
+  //       <div key={t.id} className="flex">
+  //         <div>{t.name}</div>
+  //         <Link href={`/createTimetable/${t.id}`}>select</Link>
+  //       </div>
+  //     ))}
+  // </div>
+  const id = "clhvhq0ow00077lupqaq6h5w4";
+  const data = await getTemplateTimetableById({ id });
+  const templateTimetable = data?.userTemplateTimetable;
+  const currentUser = await getCurrentUser();
+
+  if (!templateTimetable) return null;
+  if (!currentUser) return null;
+
   return (
     <div>
-      <div>createTimetable</div>
-      {templateTimetables.templateTimetables &&
-        templateTimetables.templateTimetables.map((t) => (
-          <div key={t.id} className="flex">
-            <div>{t.name}</div>
-            <Link href={`/createTimetable/${t.id}`}>select</Link>
-          </div>
-        ))}
+      <CreateTimetable
+        currentUser={currentUser}
+        templateTimetable={templateTimetable}
+      />
     </div>
   );
 }
