fb0526bc3d71fd8ad71667c55b024a8609472d3f
umaidashi <umaidashi18@gmail.com>
2023-06-18 23:50:18 +0900

    初回ページを別コンポーネントに

--- src/app/fast/First.tsx
+++ src/app/fast/First.tsx
@@ -3,12 +3,16 @@
 import { FastGroupWithAll } from "@/types/fastGroup";
 import { FastTimetableWithKomas } from "@/types/fastTimetable";
 
-export default function FastById({
+export default function First({
   fastGroup,
   fastTimetables,
 }: {
   fastGroup: FastGroupWithAll;
   fastTimetables: FastTimetableWithKomas[];
 }) {
-  return <div>hoge</div>;
+  return (
+    <div className="text-2xl font-bold text-center">
+      グループが作成されました！<br></br>早速、友人を招待しましょう！！
+    </div>
+  );
 }
--- src/app/fast/[id]/page.tsx
+++ src/app/fast/[id]/page.tsx
@@ -1,6 +1,7 @@
 import getFastGroupById from "@/app/actions/getFastGroupById";
 import FastById from "../FastById";
 import getFastTimetablesById from "@/app/actions/getFastTimetablesById";
+import First from "../First";
 
 export default async function Page({
   params,
@@ -9,18 +10,25 @@ export default async function Page({
   params: { id: string };
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const first = searchParams?.first;
+  const first = searchParams?.first === "true";
 
   const fastGroup = await getFastGroupById({ id: params.id });
   const fastTimetables = await getFastTimetablesById({ id: params.id });
 
   if (!fastGroup?.fastGroup || !fastTimetables?.fastTimetables) return;
+
+  if (first)
+    return (
+      <First
+        fastGroup={fastGroup.fastGroup}
+        fastTimetables={fastTimetables.fastTimetables}
+      />
+    );
   return (
     <div>
       <FastById
         fastGroup={fastGroup.fastGroup}
         fastTimetables={fastTimetables.fastTimetables}
-        first={first === "true"}
       />
     </div>
   );
