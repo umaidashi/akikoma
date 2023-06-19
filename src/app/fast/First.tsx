f8f70bde01823d867109f1493967860f39ce0fe6
umaidashi <umaidashi18@gmail.com>
2023-06-19 13:57:55 +0900

    Update First.tsx

--- src/app/fast/First.tsx
+++ src/app/fast/First.tsx
@@ -2,6 +2,7 @@
 
 import { FastGroupWithAll } from "@/types/fastGroup";
 import { FastTimetableWithKomas } from "@/types/fastTimetable";
+import { usePathname } from "next/navigation";
 
 export default function First({
   fastGroup,
@@ -10,9 +11,18 @@ export default function First({
   fastGroup: FastGroupWithAll;
   fastTimetables: FastTimetableWithKomas[];
 }) {
+  const path = usePathname();
   return (
-    <div className="text-2xl font-bold text-center">
-      グループが作成されました！<br></br>早速、友人を招待しましょう！！
-    </div>
+    <>
+      <div className="text-2xl font-bold text-center">
+        グループが作成されました！<br></br>早速、友人を招待しましょう！！
+      </div>
+      <div>
+        url :{" "}
+        <a
+          href={`http://localhost:3000/${path}/invite`}
+        >{`http://localhost:3000/${path}/invite`}</a>
+      </div>
+    </>
   );
 }
