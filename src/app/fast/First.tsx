936922d0f00d86eaa5c77ee1ede341b5f4e4f912
umaidashi <umaidashi18@gmail.com>
2023-06-23 00:19:07 +0900

    Update First.tsx

--- src/app/fast/First.tsx
+++ src/app/fast/First.tsx
@@ -5,6 +5,8 @@ import { FastTimetableWithKomas } from "@/types/fastTimetable";
 import { useQRCode } from "react-qrcodes";
 import { usePathname } from "next/navigation";
 import { RefObject } from "react";
+import { faClipboard, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function First({
   fastGroup,
@@ -22,11 +24,11 @@ export default function First({
       level: "H", //誤り訂正レベル
       margin: 3, //QRコードの周りの空白マージン
       scale: 1,
-      width: 200,
+      width: 350,
     },
   });
 
-  function copyTextToClipboard(text: string) {
+  const copyTextToClipboard = (text: string) => {
     navigator.clipboard.writeText(text).then(
       function () {
         console.log("Async: Copying to clipboard was successful!");
@@ -35,7 +37,7 @@ export default function First({
         console.error("Async: Could not copy text: ", err);
       }
     );
-  }
+  };
   const saveCanvas = () => {
     let canvas = document.getElementById("canvas") as HTMLCanvasElement;
     let a = document.createElement("a");
@@ -50,20 +52,26 @@ export default function First({
       <div className="text-2xl font-bold text-center">
         グループが作成されました！<br></br>早速、友人を招待しましょう！！
       </div>
-      <div>
-        url:
-        <a href={`${baseUrl}${path}/invite`}>{`${baseUrl}${path}/invite`}</a>
+      <div className="flex flex-col items-center mb-8">
+        <canvas
+          id="canvas"
+          ref={inputRef as unknown as RefObject<HTMLCanvasElement>}
+        />
+        <button className="btn btn-lg btn-primary" onClick={saveCanvas}>
+          DownLoad
+          <FontAwesomeIcon icon={faDownload} size="lg" />
+        </button>
       </div>
-      <button className="btn" onClick={() => copyTextToClipboard(`${baseUrl}${path}/invite`)}>
-        copy
-      </button>
-      <canvas
-        id="canvas"
-        ref={inputRef as unknown as RefObject<HTMLCanvasElement>}
-      />
-      <button className="btn" onClick={saveCanvas}>
-        DownLoad
-      </button>
+      <div className="flex items-center justify-center gap-4 my-4">
+        <div className="font-bold text-lg">URLで共有する</div>
+        <button
+          className="btn btn-secondary"
+          onClick={() => copyTextToClipboard(`${baseUrl}${path}/invite`)}
+        >
+          URLをコピー
+          <FontAwesomeIcon icon={faClipboard} size="lg" />
+        </button>
+      </div>
     </>
   );
 }
